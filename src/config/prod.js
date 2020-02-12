8b93786a319cee8d1a3db81bacfba5bef38030b6
femiaghedo <jaghedo@groove.ng>
2020-02-12 20:11:07 +0100

    todo

---
+++ src/config/prod.js
@@ -0,0 +1,11 @@
+  
+export const FirebaseConfig = {
+    apiKey: "AIzaSyCNLcjXwjG6H0EVSZwTyOCjWmBJScSsa-U",
+    authDomain: "food-requests.firebaseapp.com",
+    databaseURL: "https://food-requests.firebaseio.com",
+    projectId: "food-requests",
+    storageBucket: "food-requests.appspot.com",
+    messagingSenderId: "139251462283",
+    appId: "1:139251462283:web:3d1439d5c6208308aca48e",
+    measurementId: "G-5QWJBVDX2C"
+  };
