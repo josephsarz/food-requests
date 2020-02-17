71eff1d36a5d73573eac1ec6ac61bdb6e49f593d
Andy Eshiet <eandy@groove.ng>
2020-02-17 18:25:29 +0100

    refactored list item to FoodItem

--- src/components/food-items/foodItem.component.jsx
+++ src/components/food-items/foodItem.component.jsx
@@ -1,32 +1,30 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {completeToDo} from '../actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { completeToDo } from "../../actions/";
 
-class ListItem extends Component {
+class FoodItem extends Component {
   completeClick = completeTodoId => {
-    const {completeToDo} = this.props;
+    const { completeToDo } = this.props;
     completeToDo(completeTodoId);
   };
   render() {
-    const{todoId, todo} = this.props;
+    const { todoId, todo } = this.props;
     return (
       <div key="toDoName" className="col s10 card blue-grey darken-1">
-
         <div className="card-content white-text ">
           <h4>{todo.username}</h4>
           {todo.meal} <br></br>
           &#8358;{todo.amount}
-          <span 
+          <span
             onClick={() => this.completeClick(todoId)}
             className="complete-todo-item waves-effect btn"
           >
             <i className="large material-icons">Delete</i>
           </span>
         </div>
-      
       </div>
     );
   }
 }
 
-export default connect(null, {completeToDo})(ListItem);
+export default connect(null, { completeToDo })(FoodItem);
