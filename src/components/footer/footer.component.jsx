bf137a82d47b3044a9c7d51a277ccf38451820df
Andy Eshiet <eandy@groove.ng>
2020-02-18 13:15:02 +0100

    added Footer

---
+++ src/components/footer/footer.component.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const Footer = () => (
+  <footer className="footer">
+    <div className="footer-copyright">
+      <div className="container">© 2020 Copyright Android Team</div>
+
+      <div className="col l4 offset-l2 s12">
+        <ul>
+          <li>
+            <a
+              color="#00000000"
+              className="grey-text text-lighten-4 right"
+              href="http://github.com/josephsarz"
+            >
+              Femicodes
+            </a>
+          </li>
+          <li>
+            <a
+              className="grey-text text-lighten-4 right"
+              href="http://github.com/mathemandy"
+            >
+              Mathemandy
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+);
--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
 import "materialize-css/dist/css/materialize.min.css";
 import "materialize-css/dist/js/materialize.min.js";
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
-import reduxThunk from 'redux-thunk';
-import reducers from './reducers';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reduxThunk from "redux-thunk";
+import reducers from "./reducers";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Footer } from "./components/footer/footer.component";
+import "./index.css";
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
-ReactDOM.render(
+// fix for the footer
+// https://medium.com/@stefanmaretic/react-sticky-footer-f842d5fbd68
+
+const withFooter = WrappedComponent => () => [
+  <WrappedComponent key="1" />,
+  <Footer key="2" />
+];
+
+const Wrapper = () => (
   <Provider store={store}>
     <App />
-  </Provider>,
-  document.getElementById("root")
+  </Provider>
 );
+
+const WrapperWithFooter = withFooter(Wrapper);
+
+ReactDOM.render(<WrapperWithFooter />, document.getElementById("root"));
+
 serviceWorker.unregister();
